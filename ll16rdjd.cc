ec69b94558e324201ece25c771852aa713bba110
friengineer <174277232+friengineer@users.noreply.github.com>
2024-07-15 22:37:25 +0100

    Indent code and add spaces

--- ll16rdjd.cc
+++ ll16rdjd.cc
@@ -1,45 +1,43 @@
 #include "globals.h"
 #include "gitpp.h"
-
 #include <QtWidgets>
 
-namespace{
-
+namespace {
 	GITPP::REPO r("..");
 
 	// class definition for a branch tab
-	class BranchesTab : public QWidget{
-		Q_OBJECT
+	class BranchesTab : public QWidget {
+			Q_OBJECT
 
-	public:
-		BranchesTab();
+		public:
+			BranchesTab();
 
-	private:
-		void createWidgets();
-		void arrangeWidgets();
-		void makeConnections();
+		private:
+			void createWidgets();
+			void arrangeWidgets();
+			void makeConnections();
 
-		QLabel* titleLabel;
-		QLabel* mainText;
-		QLabel* branchLabel;
-		QComboBox* branchSelector;
-		QPushButton* selectButton;
-		QMessageBox* switched;
-		QMessageBox* error;
+			QLabel* titleLabel;
+			QLabel* mainText;
+			QLabel* branchLabel;
+			QComboBox* branchSelector;
+			QPushButton* selectButton;
+			QMessageBox* switched;
+			QMessageBox* error;
 
-	private slots:
-		void switchBranch();
+		private slots:
+			void switchBranch();
 	};
 
 	// constructor
-	BranchesTab::BranchesTab(){
+	BranchesTab::BranchesTab() {
 		createWidgets();
 		arrangeWidgets();
 		makeConnections();
 	}
 
 	// creates the widgets for the tab
-	void BranchesTab::createWidgets(){
+	void BranchesTab::createWidgets() {
 		titleLabel = new QLabel("Branches");
 		mainText = new QLabel("Select which branch you would like to view.");
 
@@ -54,13 +52,13 @@ namespace{
 		branchLabel->setAlignment(Qt::AlignCenter);
 
 		// populates the combo box with the branch names
-		for(GITPP::BRANCH i : r.branches()){
+		for (GITPP::BRANCH i : r.branches()) {
 			branchSelector->addItem(QString::fromStdString(i.name()));
 		}
 	}
 
 	// arranges the layout of the tab
-	void BranchesTab::arrangeWidgets(){
+	void BranchesTab::arrangeWidgets() {
 		QVBoxLayout* mainLayout = new QVBoxLayout;
 
 		mainLayout->addWidget(titleLabel);
@@ -73,18 +71,19 @@ namespace{
 
 	// switches to the selected branch and displays message boxes to give feedback
 	// on the status
-	void BranchesTab::switchBranch(){
+	void BranchesTab::switchBranch() {
 		QString selectedBranch = branchSelector->currentText();
 		std::string branchName = selectedBranch.toUtf8().constData();
 
-		try{
+		try {
 			r.checkout(branchName);
 			switched = new QMessageBox();
 			switched->setWindowTitle("Branch Switched");
 			switched->setText("You have switched to the " + selectedBranch + " branch.");
 
 			switched->exec();
-		}catch(GITPP::EXCEPTION_CANT_FIND const& e){
+		}
+		catch (GITPP::EXCEPTION_CANT_FIND const& e) {
 			error = new QMessageBox();
 			error->setWindowTitle("Branch Not Switched");
 			QString message = "An error occurred while switching to the ";
@@ -94,7 +93,8 @@ namespace{
 			error ->setText(message + selectedBranch + message2 + message3 + message4);
 
 			error->exec();
-		}catch(GITPP::EXCEPTION const& e){
+		}
+		catch (GITPP::EXCEPTION const& e) {
 			error = new QMessageBox();
 			error->setWindowTitle("Branch Not Switched");
 			QString message = "An error occurred while switching to the ";
@@ -108,12 +108,11 @@ namespace{
 	}
 
 	// connects widgets
-	void BranchesTab::makeConnections(){
+	void BranchesTab::makeConnections() {
 		connect(selectButton, SIGNAL(clicked()), this, SLOT(switchBranch()));
 	}
 
 	INSTALL_TAB(BranchesTab, "branches");
-
 }
 
 #include "ll16rdjd.moc"
